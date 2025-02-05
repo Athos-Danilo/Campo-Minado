24a830c36490ec1535b8fa261d1cc56c0cf3230e
Athos <athos-danilo@outlook.com>
2025-02-05 16:01:37 -0300

    Quinto Commit

--- Jogo.py
+++ Jogo.py
@@ -1,12 +1,18 @@
 def facil():
-    linhas = 9 
+    linhas = 9
     colunas = 9
     matriz_facil = []
-    for contador in range(linhas):
+
+    for contador_um in range(linhas):
         linha = []
-    for contador in range(colunas):
-        linha.append(0)
-    matriz_facil.append(linha)
+        for contador_dois in range(colunas):
+            linha.append("■")
+        matriz_facil.append(linha)
+        
+    for linha in matriz_facil:
+        for elemento in linha:
+            print(elemento, end=' ')
+        print() 
 
 print("")
 print("~~~~~~~~~~~~-> \033[32m CAMPO MINADO \033[0m <-~~~~~~~~~~~~")
@@ -17,14 +23,14 @@ print("[2] Médio")
 print("[3] Difícil")
 while True:
     try:
-        nivel = int(input(": "))
+        nivel = int(input("Resposta: "))
         if nivel in [1, 2, 3]:
             break
         else:
             print("Só existem três níveis! Digite 1, 2 ou 3.")
     except ValueError:
         print("Digite um Número Válido!")
-
-
-
+print("--------------------------------------------")
+if nivel == 1:
+    facil()
 
--- Área de Testes.py
+++ Área de Testes.py
@@ -29,14 +29,41 @@
 
 
 
-def facil():
-    linhas = 9
-    colunas = 9
-    matriz_facil = []
-    for contador in range(linhas):
-        linha = []
-        for contador in range(colunas):
-            linha.append(0)
-        matriz_facil.append(linha)
-    return matriz_facil
-print(facil())
+# def facil():
+#     linhas = 9
+#     colunas = 9
+#     matriz_menor = []
+
+#     for contador_um in range(linhas):
+#         linha = []
+#         for contador_dois in range(colunas):
+#             linha.append(0)
+#         matriz_menor.append(linha)
+
+#     return matriz_menor
+
+# matriz_facil = facil()
+
+# for linha in matriz_facil:
+#     for elemento in linha:
+#         print(elemento, end=' ')
+#     print()    
+
+
+# def facil():
+#     linhas = 9
+#     colunas = 9
+#     matriz_menor = []
+
+#     for contador_um in range(linhas):
+#         linha = []
+#         for contador_dois in range(colunas):
+#             linha.append("■")
+#         matriz_menor.append(linha)
+        
+#     for linha in matriz_menor:
+#         for elemento in linha:
+#             print(elemento, end=' ')
+#         print()    
+
+# facil()
