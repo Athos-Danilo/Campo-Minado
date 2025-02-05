cacf440d22c9d1047efaca01e7e2ddbd5128a0a2
Athos <athos-danilo@outlook.com>
2025-02-05 13:12:28 -0300

    Teceiro Commit

--- Jogo.py
+++ Jogo.py
@@ -4,5 +4,13 @@ print("ESCOLHA O NÍVEL DE DIFICULDADE:")
 print("[1] Fácil")
 print("[2] Médio")
 print("[3] Difícil")
-nivel = input(": ")
+while True:
+    try:
+        nivel = int(input(": "))
+        if nivel in [1, 2, 3]:
+            break
+        else:
+            print("Só existem três níveis! Digite 1, 2 ou 3.")
+    except ValueError:
+        print("Digite um Número Válido!")
 
