44d8a06034a9415bf7be633833d0429dd564ed72
Athos <athos-danilo@outlook.com>
2025-02-05 12:48:11 -0300

    Segundo Commit

---
+++ Jogo.py
@@ -0,0 +1,8 @@
+print("~~~~~~~~~~-> CAMPO MINADO <-~~~~~~~~~~")
+print("--------------------------------------")
+print("ESCOLHA O NÍVEL DE DIFICULDADE:")
+print("[1] Fácil")
+print("[2] Médio")
+print("[3] Difícil")
+nivel = input(": ")
+
